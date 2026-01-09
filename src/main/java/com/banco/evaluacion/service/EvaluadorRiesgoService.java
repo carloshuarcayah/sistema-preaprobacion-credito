cd9c463446209f14d26ccffa3d798366427317f8
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-08 21:41:20 -0500

    service: EvaluadorRiesgo agregado, test: EvaluadorRiesgoTest agregado

---
+++ src/main/java/com/banco/evaluacion/service/EvaluadorRiesgoService.java
@@ -0,0 +1,54 @@
+package com.banco.evaluacion.service;
+//DEBE SER MAYOR DE EDAD PARA PEDIR PRESTAMO Y NO SUPERAR LOS 70 AÑOS(RIESGO)
+//SCORE CREDITICIO MAYOR A 50 ( PODRIAMOS HACER QUE CADA TIPO DE PRESTAMO TENGA SU PROPIO SCORE MINIMO)
+//PRESTAMO MINIMO DE 500 SOLES. (SI PIDE EN OTRA MONEDA QUE NO SEA SOLES IGUAL, DEBE SER MAYOR A 500 SOLES AL PASARLO)
+//NO DEBE TENER DEUDAS
+//EL PRESTAMO NO DEBERIA SUPERAR EL 30% DE SU SUELDO NETO
+
+//DE MOMENTO SOLO VALIDAREMOS EDAD, SCORE, SI TIENE DEUDA, Y QUE NO SUPERE 30% DE SU SUELDO
+
+import com.banco.evaluacion.exception.PreAprobacionException;
+import com.banco.evaluacion.model.Cliente;
+import com.banco.evaluacion.model.Prestamo;
+
+public class EvaluadorRiesgoService {
+    private final CalculadoraPrestamo calculadoraPrestamo = new CalculadoraPrestamo();
+    private final static double PRESTAMO_MINIMO = 500.0;
+    private final static int EDAD_MINIMA = 18;
+    private final static int EDAD_MAXIMA = 70;
+    private final static int SCORE_MINIMO = 50;
+
+    public void validarPrestamo(Cliente cliente, Prestamo prestamo){
+
+        //TIENE QUE SER MAYOR DE EDAD Y NO TENER MAS DE 70
+        if(cliente.edad()<EDAD_MINIMA||cliente.edad()>EDAD_MAXIMA){
+            throw new PreAprobacionException("Error edad: El cliente no cumple con la edad permitida.");
+        }
+
+        //TIENE DEUDA
+        if(cliente.tieneDeuda()){
+            throw new PreAprobacionException("Error deuda: El cliente tiene una deuda pendiente.");
+        }
+
+        //NO CUMPLE CON EL SCORE MINIMO
+        if(cliente.historialCrediticio()<SCORE_MINIMO){
+            throw new PreAprobacionException("Error crediticio: El cliente no cumple con el score minimo.");
+        }
+
+        //PRESTAMO MENOR AL MINIMO
+        if(prestamo.monto()<PRESTAMO_MINIMO){
+            throw new PreAprobacionException("Error monto: El monto solicitado es menor al minimo permitido");
+        }
+
+        double limitePrestamo = cliente.sueldoNeto()*0.3;
+        double cuotaMensual = calculadoraPrestamo.calcularCuotaMensual(prestamo);
+        boolean prestamoValido = cuotaMensual<limitePrestamo;
+
+        //SI EL PRESTAMO SUPERA EL 30% DEL SUELDO NETO
+        if(!prestamoValido){
+            throw new PreAprobacionException("Error crediticio: La cuota mensual de S/."+cuotaMensual+", es demasiado alta para un sueldo de s/."+cliente.sueldoNeto());
+        }
+
+        System.out.println("Validacion exitosa: ¡Su credito ha sido aprobado!");
+    }
+}
---
+++ src/test/java/com/banco/evaluacion/service/EvaluadorRiesgoTest.java
@@ -0,0 +1,61 @@
+package com.banco.evaluacion.service;
+
+import com.banco.evaluacion.exception.PreAprobacionException;
+import com.banco.evaluacion.model.Cliente;
+import com.banco.evaluacion.model.Prestamo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class EvaluadorRiesgoTest {
+    EvaluadorRiesgoService evaluadorRiesgoService;
+    @BeforeEach
+    void inicializar(){
+        evaluadorRiesgoService= new EvaluadorRiesgoService();
+    }
+
+    //TESTS A PROBAR
+    //ES MENOR DE EDAD O SUPERA LOS 70, LANZA ERROR
+    //ES MAYOR DE EDAD, OK
+    @ParameterizedTest
+    @CsvSource({
+            "15, INVALIDO",
+            "17, INVALIDO",
+            "18, VALIDO",
+            "46, VALIDO",
+            "70, VALIDO",
+            "75, INVALIDO",
+    })
+    void testValidarPorEdad(int edad, String estado){
+        Cliente cliente = new Cliente("Luis",edad,90,3000,false);
+        Prestamo prestamo = new Prestamo(600,12,"HIPOTECARIO");
+
+        switch (estado.toUpperCase().trim()){
+            case "VALIDO"->{
+                assertDoesNotThrow(()->{
+                    evaluadorRiesgoService.validarPrestamo(cliente,prestamo);
+                },"Se esperaba que la edad:"+edad+" sea valida.");
+            }
+            case "INVALIDO"->{
+                assertThrows(PreAprobacionException.class,()->{
+                    evaluadorRiesgoService.validarPrestamo(cliente,prestamo);
+                },"Se esperaba que la edad:"+edad+" sea invalido.");
+            }
+        }
+    }
+
+    //TIENE DEUDA,  ERROR
+    //NO TIENE DEUDA, OK
+
+    //PIDE UN PRESTAMO MENOR AL MONTO MINIMO, ERROR
+    //PIDE UN PRESTAMO MAYOR AL MINIMO, OK
+
+    //SCORE CREDITICIO MENOR AL MINIMO, ERROR
+    //SCORE CREDITICIO SUPERIOR AL MINIMO, OK
+
+    //PRESTAMO SUPERA EL 30% DEL SUELDO NETO, ERROR
+    //PRESTAMO NO SUPERA EL 30% DEL SUELDO TENO, OK
+
+}
