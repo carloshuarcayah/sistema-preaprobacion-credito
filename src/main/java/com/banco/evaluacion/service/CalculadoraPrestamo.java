bbcc4e0a35b2fbb0a62686477a1ad51f3a40a89c
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-16 16:51:17 -0500

    Agregamos validaciones antes de realizar los calculos

--- src/main/java/com/banco/evaluacion/service/CalculadoraPrestamo.java
+++ src/main/java/com/banco/evaluacion/service/CalculadoraPrestamo.java
@@ -6,8 +6,10 @@
     //AQUI VIENE LA LOGICA QUE USAREMOS PARA EL CALCULO DE INTERESES SEGUN EL TIPO DE PRESTAMO
     public class CalculadoraPrestamo {
         public double calcularCuotaMensual(Prestamo prestamo){
-            double interes = prestamo.tipoPrestamo().getInteres();
+            if(prestamo==null) throw new PreAprobacionException("Error de dato: Objeto invalido o null");
+            if(prestamo.monto()<0) throw new PreAprobacionException("Error al calcular: Se ha introducido un monto invalido");
 
+            double interes = prestamo.tipoPrestamo().getInteres();
             return (interes* prestamo.monto())/ prestamo.plazoMeses();
         }
     }
